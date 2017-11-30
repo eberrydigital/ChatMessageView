ee2f35ff0b617ee55b5d0c52e6b7a2abd2b5cede
sewillguler <sevillguler@gmail.com>
2017-11-30 12:08:59 +0100

    Compilation error

--- chatmessageview/src/main/kotlin/com/eberrydigital/chatview/util/DateFormatter.kt
+++ chatmessageview/src/main/kotlin/com/eberrydigital/chatview/util/DateFormatter.kt
@@ -8,6 +8,6 @@ import java.util.*
  */
 class DateFormatter : ITimeFormatter {
     override fun getFormattedTimeText(createdAt: String): String {
-        return TimeUtils.dateToString(createdAt, "MMM. dd, yyyy")
+        return TimeUtils.dateToString(createdAt, "yyyy-MMM-dd HH:mm:ss")
     }
 }
